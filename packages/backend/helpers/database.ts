5239d8ca39621bdd4bf50eb20fd8fe6ffebeac53
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2022-07-24 21:47:33 +0100

    added DB helper

---
+++ packages/backend/helpers/database.ts
@@ -0,0 +1,32 @@
+import Mongoose from "mongoose";
+import logger from "../utils/logger";
+    
+class Database {
+    static mongooseInstance: any;
+    static mongooseConnection: Mongoose.Connection;
+    private _url: string;
+    
+    constructor (public url: string) {
+      this._url = url;
+      Database.connect(this._url);
+    }
+    
+    static async connect (url:string): Promise<Mongoose.Connection> {
+      if(this.mongooseInstance) return this.mongooseInstance;
+      
+      this.mongooseConnection  = Mongoose.connection;
+      this.mongooseConnection.once("open", () => {
+        logger.info('MongoDB database connected');
+      });
+      
+      this.mongooseInstance = await Mongoose.connect(url, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useFindAndModify: false,
+      // useCreateIndex: true
+      });
+      return this.mongooseInstance;
+    }   
+};
+
+export default Database;
