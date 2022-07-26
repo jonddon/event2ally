37b14c298c8306bee30a5f80e219e60a68641e18
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2022-07-26 23:00:28 +0100

    updated `Event` model

--- packages/backend/app/models/Event.ts
+++ packages/backend/app/models/Event.ts
@@ -12,22 +12,44 @@ enum EventCategory {
   Webinar
 }
 
+interface IEventLocation {
+  address: string;
+  city: string;
+  country: string;
+  latitude: string;
+  longitude: string
+}
+
+const EventLocationType = {
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  latitude: { type: String, required: true },
+  longitude: { type: String, required: true },
+}
+
 interface IEvent {
-  host: string;
   name: string;
-  category: string;
+  host: string;
+  creator: string;
+  contract_address: string;
+  date: Date;
   type: string;
-  address?: string;
-  ticket?: string;
+  category: string;
+  location?: IEventLocation;
+  ticket_price?: string;
 }
 
 const eventSchema = new Schema<IEvent>({
   name: { type: String, required: true },
   host: { type: String, required: true },
+  creator: { type: String, required: true },
+  contract_address: { type: String, unique: true, required: true },
+  date: { type: Date, required: true },
   type: { type: String, enum: EventType, required: true },
   category: { type: String, enum: EventCategory, required: true },
-  address: { type: String, required: false },
-  ticket: { type: String, required: false },
+  location: EventLocationType,
+  ticket_price: { type: String, required: false },
 });
 
 const EventModel = model<IEvent>('Event', eventSchema);
