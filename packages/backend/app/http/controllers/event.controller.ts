4dc97217cc8413b4e8cf193ef401f32aae6b95e4
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2022-08-16 01:46:37 +0100

    fix: error handling

--- packages/backend/app/http/controllers/event.controller.ts
+++ packages/backend/app/http/controllers/event.controller.ts
@@ -1,19 +1,21 @@
-import { Request, Response } from 'express';
-import { FailedResponse, SuccessResponse, ServerErrorResponse } from '../../../utils/response';
+import { NextFunction, Request, Response } from 'express';
+import { SuccessResponse, ServerErrorResponse } from '../../../utils/response';
 import EventRepository from '../../repository/event.repository';
-import logger from '../../../utils/logger';
+import { logger } from '../../../utils/logger';
 
 
 export default class EventController {
   constructor() {};
 
-  public async createEvent(req: Request, res: Response): Promise<void> {
+  public async createEvent(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const event = await EventRepository.create(req.body);
-      return SuccessResponse(event, 200, "event created", null);
+      logger.info(event);
+      return SuccessResponse(res, 200, "event created", event);
     } catch (err) {
       logger.error(err);
-      return ServerErrorResponse(res, 500, 'Server Error: contact admin', {});
+      next(err);
+      // return ServerErrorResponse(res, 500, 'Server Error: contact admin', {});
     }
   }
 };
--- packages/backend/app/http/errors/error.handler.ts
+++ packages/backend/app/http/errors/error.handler.ts
@@ -2,7 +2,7 @@ import { Application, Request, Response, NextFunction } from 'express';
 import { errors, isCelebrateError } from "celebrate";
 import { MongoError } from 'mongodb';
 import { NotFoundError } from './app.errors';
-import logger from '../../../utils/logger';
+import { logger as log } from '../../../utils/logger';
 import { FailedResponse, ServerErrorResponse } from '../../../utils/response';
 
 export default function (app: Application) {
@@ -17,46 +17,37 @@ export default function (app: Application) {
   });
 
   // Request error handler
-  app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
-    console.log('error ====>', err.message);
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const userString = 'unknown user';
     if (err instanceof SyntaxError) {
-      return FailedResponse(res, 400, 'Bad Request', null)
+      return FailedResponse(res, 400, 'Bad Request', null);
     }
-    if (isCelebrateError(err)) {
-      const messages =  err.details;
+    else if (isCelebrateError(err)) {
+      const messages = err.details;
       let jsonObject: any = {};  
-      messages.forEach((value: any, key: any) => {jsonObject[key] = value});  
+      messages.forEach((value: any, key: any) => { jsonObject[key] = value });  
       let msg = jsonObject.body.details.map((er: any)=>{ return {[er.path]: er.message.replace(/["]+/g, '')}});
-			const message = jsonObject.body.details ? jsonObject.body.details[0].message.replace(/["]+/g, '') : err.message;
+      const message = jsonObject.body.details ? jsonObject.body.details[0].message.replace(/["]+/g, '') : err.message;
       // console.log('json==>', msg, messages, message);
       return FailedResponse(res, 400, message, msg);
     }
-    if (err.message && err.code) {
-      return FailedResponse(res, err.code, err.message, err);
-    }
-    app.use(errors());
-    return next(err);
-  });
-
-  // Log all errors
-  app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
-    const userString = 'unknown user';
-    if (err instanceof MongoError) {
+    else if (err instanceof MongoError) {
       if (err.code === 11000) {
-        logger.error(`${req.method} ${req.path}: MongoDB duplicate entry from ${userString}`);
+        log.error(`${req.method} ${req.path}: MongoDB duplicate entry from ${userString}`);
       } else {
-        logger.error(`${req.method} ${req.path}: Unhandled MongoDB error ${userString}. ${err.errmsg}`);
+        log.error(`${req.method} ${req.path}: Unhandled MongoDB error ${userString}. ${err.errmsg}`);
       }
       if (!res.headersSent) {
         return res.sendStatus(500);
       }
-    } else if (err instanceof Error) {
-      logger.error(`${req.method} ${req.path}: Unhandled request error ${userString}. ${err.message}`);
-    } else if (typeof err === 'string') {
-      logger.error(`${req.method} ${req.path}: Unhandled request error ${userString}. ${err}`);
-    } 
-    console.log(err);
-    // logger.error(`${req.method} ${req.path}: Request error ${userString}. ${err}`);
+    }
+    if (err instanceof Error) {
+      log.error(`${req.method} ${req.path}: Unhandled request error ${userString}. ${err.message}`);
+    }
+    if (typeof err === 'string') {
+      log.error(`${req.method} ${req.path}: Unhandled request error ${userString}. ${err}`);
+    }
+    app.use(errors());
     return next(err);
   });
 
